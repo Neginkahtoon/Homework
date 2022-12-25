7f543d3aec574da1ce2f92ca1da9c2a4531065cd
Neginkahtoon <121246964+Neginkahtoon@users.noreply.github.com>
2022-12-25 04:29:53 -0800

    Update analyze.py

--- analyze.py
+++ analyze.py
@@ -1,45 +1,46 @@
 import re
 import xlsxwriter
+
 opDic= {
     ('STRING_LITRAL', r'".*"'),
-    ('COMMENT_STARTER',"#"),
-    ('LBRACKET', '('),       
-    ('RBRACKET', ')'),       
-    ('LBRACE', '{'),         
-    ('RBRACE', '}'),         
-    ('COMMA', ','),            
-    ('SEMICOLON', ';'),        
-    ('COLON',':'),            
-    ('EQ', '=='),             
-    ('NE', '!='),             
-    ('LE', '<='),             
-    ('GE', '>='),             
-    ('LT', '<'),              
-    ('GT', '>'),              
-    ('NOT', 'not'),
-    ('OR', 'or'),           
-    ('AND', 'and'),             
-    ('ASSIGNMENT', '='),
-    ('PLUS', '+'),('PLUS', '+='),           
-    ('MINUS', '-'),('MINUS', '-='),   
-    ('MULT', '*'),('MULT', '*='),           
-    ('DIV', '/'),('DIV', '/='),
-    ('MOD', '%'),('MOD', '%='),
-    ('FLOORDIV', '//'),('FLOORDIV', '//='), 
-    ('POw','**'),('POw','**='),
-    ('LEFTSHIFT',"<<"),('LEFTSHIFT',"<<="),
-    ('RIGHTSHIFT',">>"),('RIGHTSHIFT',">>="),
-    ('BitwiseXOR',"^"),('BitwiseXOR',"^="),
-    ('BitwiseOR',"|"),('BitwiseOR',"|="),
-    ('BitwiseAND',"&"),('BitwiseAND',"&="),
-    ('FLOAT_CONST', r'\d(\d)*\.\d(\d)*'),   # FLOAT
-    ('INTEGER_CONST', r'\d(\d)*'),          # INT
-    ('NEWLINE', r'\n'),         # NEW LINE
-    ('SKIP', r'[ \t]+'),        # SPACE and TABS
-    ('MISMATCH', r'.'),         
-    ('IDENTITY', 'is'), ('IDENTITY', 'is not'),
-    ('MemBERSHIP', 'in'), ('MemBERSHIP', 'not in'),
-    ('ID', r'^[^0-9]\w*'),
+    ('COMMENT_STARTER','#'),
+    
+    ('LBRACKET', '\('),
+    ('RBRACKET', '\)'),
+    ('LBRACE', '\{'),
+    ('RBRACE', '\}'),
+    ('COMMA', ','),
+    ('SEMICOLON', ';'),
+    ('COLON',':'),
+    ('EQ', '=='),
+    ('NE', '!='),
+    ('LE', '<='),
+    ('GE', '>='),
+    ('LT', '<'),
+    ('GT', '>'),
+
+    ('PLUS', '\+[\=]?'),
+    ('MINUS', '-[\=]?'),
+    ('MULT', '\*[\=]?'),
+    ('DIV', '\/[\=]?'),
+    ('MOD', '%[\=]?'),
+    ('FLOORDIV', '\/\/[\=]?'),
+    ('POw','\*\*[\=]?'),
+    ('LEFTSHIFT',"<<[\=]?"),
+    ('RIGHTSHIFT',">>[\=]?"),
+    ('BitwiseXOR',"\^[\=]?"), 
+    ('BitwiseOR',"\|[\=]?"),
+    ('BitwiseAND',"&[\=]?"),
+
+    ('FLOAT_CONST', r'\d(\d)*\.\d(\d)*'),
+    ('INTEGER_CONST', r'\d(\d)*'),
+    
+    ('NEWLINE', r'\n'),
+    ('SKIP', r'[ \t]+'),
+
+    ('ASSIGNMENT','\='),
+    ('ID', r'[a-zA-Z_][0-9a-zA-Z_]*'),
+   
     }
 
 keywordList = ['and', 'as', 'assert', 'break', 'class', 'def', 'del',
@@ -52,51 +53,51 @@ def isKeyword(str):
 
 
 def main():
+    print("(STARTMARKER)")
+    #path = input("Enter path of input scipt file:")
+    #outpath = input("Enter path of output excel file:")
+    path="c:/1.py"
+    outpath="c:/1.xlsx"
+    f = open(path, 'r')
+    lines = f.readlines()
+    if len(lines) <= 0:
+        print("Input file %s is empty" % f)
+        quit(0)
+    buf=''
+    for line in lines:
+        buf+=line  
     
-        path = input("Enter path of input scipt file:")
-        outpath = input("Enter path of output excel file:")
+    tokens_join = '|'.join('(?P<%s>%s)' % x for x in opDic)
+
+    workbook = xlsxwriter.Workbook(outpath)
+    worksheet = workbook.add_worksheet()
+    worksheet.write(0, 0, "sentence")
+    worksheet.write(0, 1, "type")
+    row=2
+    commentFLAG = False
+    for item in re.finditer(tokens_join, buf):
+        tokenType = item.lastgroup
+        token = item.group(tokenType)
         
-        f = open(path, 'r')
-        lines = f.readlines()
-        if len(lines) <= 0:
-            print("Input file %s is empty" % f)
-            quit(0)
-        buf=''
-        for line in lines:
-            buf+=line  
-        
-        tokens_join = '|'.join('(?P<%s>%s)' % x for x in opDic)
+        if ((tokenType == 'NEWLINE') and (commentFLAG==True)):
+            commentFLAG = False
+            continue;
+        elif ((tokenType == 'SKIP') or(tokenType == 'NEWLINE')):
+            continue;
+        elif tokenType== "COMMENT_STARTER":
+            commentFLAG = True
+        elif(commentFLAG==True):
+            tokenType="COMMENT"
+        elif (tokenType=="ID" and isKeyword(token)):
+            tokenType="KEYWORD"
 
-        workbook = xlsxwriter.Workbook(outpath)
-        worksheet = workbook.add_worksheet()
-        row=0
-        commentFLAG = False
-        for item in re.finditer(tokens_join, buf):
-            tokenType = item.lastgroup
-            token = item.group(type)
-            
-            if ((tokenType == 'NEWLINE') and (commentFLAG==True)):
-                commentFLAG = False
-            if tokenType== "COMMENTSTARTER":
-                commentFLAG = True
-            elif(commentFLAG==True):
-                worksheet.write(row, 1, token)
-                worksheet.write(row, 2, "COMMENT")
-                row += 1
-                continue;
-            elif tokenType == 'SKIP':
-                continue;
-            elif (tokenType=="ID" and isKeyword(token)):
-                worksheet.write(row, 1, token)
-                worksheet.write(row, 2, "KEYWORD")
-                row += 1    
-            else:
-                worksheet.write(row, 1, token)
-                worksheet.write(row, 2, tokenType)
-                row += 1
-        print("(ENDMARKER)")
-
-
+           
+        worksheet.write(row, 0, repr(token))
+        worksheet.write(row, 1, tokenType)
+        row += 1
+    print(str(row-2) + " token extracted...")
+    print("(ENDMARKER)")
+    workbook.close()
 
 
 if __name__ == "__main__":
